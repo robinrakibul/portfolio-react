83c3369eb5fecd9a804a4c1b7fa1c45ab4664981
robinrakibul <robinrakibulhassan@gmail.com>
2022-06-08 21:33:19 +0600

    added github icon to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="text-center bg-gray-900 text-white">
       <div className="container px-6 pt-6">
         <div className="flex justify-center">
-          <a href="https://www.facebook.com/robinrakibul" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-gray-600 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+          <a href="https://www.facebook.com/robinrakibul" target="_blank" rel="noreferrer" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-gray-600 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
             <svg aria-hidden="true"
               focusable="false"
               data-prefix="fab"
@@ -22,7 +22,7 @@ const Footer = () => {
             </svg>
           </a>
 
-          <a href="https://www.instagram.com/robinrakibulhassan" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-gray-600 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+          <a href="https://www.instagram.com/robinrakibulhassan" target="_blank" rel="noreferrer" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-gray-600 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
             <svg aria-hidden="true"
               focusable="false"
               data-prefix="fab"
@@ -39,7 +39,7 @@ const Footer = () => {
             </svg>
           </a>
 
-          <a href="https://www.linkedin.com/in/robinrakibul" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-gray-600 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+          <a href="https://www.linkedin.com/in/robinrakibul" target="_blank" rel="noreferrer" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-gray-600 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
             <svg aria-hidden="true"
               focusable="false"
               data-prefix="fab"
@@ -52,6 +52,22 @@ const Footer = () => {
               <path
                 fill="currentColor"
                 d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+              ></path>
+            </svg>
+          </a>
+          <a href="https://www.github.com/robinrakibul" type="button" target="_blank" rel="noreferrer" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-gray-600 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+            <svg aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="github"
+              className="w-4 h-full mx-auto"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"
               ></path>
             </svg>
           </a>
