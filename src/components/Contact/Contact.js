d7e5aace2de08a3496987ff64eaae4d566832158
robinrakibul <robinrakibulhassan@gmail.com>
2022-06-08 21:19:48 +0600

    changed contact component style

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -26,42 +26,42 @@ const Contact = () => {
               </div>
             </div>
 
-            <form className="w-full md:ml-16 lg:w-full max-w-xl" action="https://formspree.io/f/xknykqzy"
-              method="POST">
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                  <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                    Your Name
-                  </label>
-                  <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="" name="name" required />
-                  <p className="text-gray-600 text-xs italic">Enter Your Full Name</p>
+              <form className="w-full md:ml-16 lg:w-full max-w-xl bg-white p-5 rounded-2xl h-1/3" action="https://formspree.io/f/xknykqzy"
+                method="POST">
+                <div className="flex flex-wrap -mx-3 mb-6">
+                  <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Your Name
+                    </label>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="" name="name" required />
+                    <p className="text-gray-600 text-xs italic">Enter Your Full Name</p>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3">
-                  <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                    E-mail
-                  </label>
-                  <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" name="email" required />
-                  <p className="text-gray-600 text-xs italic">Enter Your Email Address</p>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                  <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      E-mail
+                    </label>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" name="email" required />
+                    <p className="text-gray-600 text-xs italic">Enter Your Email Address</p>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3">
-                  <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                    Message
-                  </label>
-                  <textarea className="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" name="message" required></textarea>
-                  <p className="text-gray-600 text-xs italic">Enter Your Inquiry</p>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                  <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Message
+                    </label>
+                    <textarea className="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" name="message" required></textarea>
+                    <p className="text-gray-600 text-xs italic">Enter Your Inquiry</p>
+                  </div>
                 </div>
-              </div>
-              <div className="md:flex md:items-center">
-                <div className="md:w-1/3">
-                  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-3.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" data-mdb-ripple="true" data-mdb-ripple-color="light">Send Message</button>
+                <div className="md:flex md:items-center">
+                  <div className="md:w-1/3">
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-3.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" data-mdb-ripple="true" data-mdb-ripple-color="light">Send Message</button>
+                  </div>
+                  <div className="md:w-2/3"></div>
                 </div>
-                <div className="md:w-2/3"></div>
-              </div>
-            </form>
+              </form>
           </div>
         </div>
       </section>
