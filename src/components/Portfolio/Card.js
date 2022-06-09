cdfd01afd76fa8e7cf86f42a13a5aa974fade917
robinrakibul <robinrakibulhassan@gmail.com>
2022-06-09 23:18:51 +0600

    fixes previous modal issue.

--- src/components/Portfolio/Card.js
+++ src/components/Portfolio/Card.js
@@ -25,7 +25,10 @@ const Card = (props) => {
           </label>
         </div>
         <div className='title'>
-          <h2>{props.title}</h2>
+          <h2 onClick={toggleModal}>{props.title}</h2>
+          <a href='#popup' className='arrow' onClick={toggleModal}>
+            <i className='fas fa-arrow-right'></i>
+          </a>
         </div>
         <a href={props.live_url} target="_blank" rel="noreferrer">
           <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-600 to-blue-500 group-hover:from-blue-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
@@ -36,7 +39,7 @@ const Card = (props) => {
         </a>
       </div>
 
-      {/* Popup box
+      {/* Popup box */}
       {modal && (
         <div className='modal'>
           <div onClick={toggleModal} className='overlay'></div>
@@ -59,7 +62,7 @@ const Card = (props) => {
             </div>
           </div>
         </div>
-      )} */}
+      )}
     </>
   )
 }
