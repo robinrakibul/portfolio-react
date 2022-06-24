293ab591b6e33f9f503281c095c84832b2b4a2d7
robinrakibul <robinrakibulhassan@gmail.com>
2022-06-24 17:40:55 +0600

    Portfolio data updated

--- src/components/Resume/ResumeApi.js
+++ src/components/Resume/ResumeApi.js
@@ -5,7 +5,6 @@ const ResumeApi = [
     institute: "East West University",
     year: 2021,
     major: 'Computer Science And Engineering (CSE)',
-    rate: "CGPA 2.77/4.00",
     exam: "Bachelor Of Science (BSC)"
   },
   {
@@ -14,7 +13,6 @@ const ResumeApi = [
     institute: "Ideal College, Dhanmondi",
     year: 2016,
     major: 'Science',
-    rate: "GPA 4.75/5.00",
     exam: "Higher Secondary Certificate (HSC)"
   },
   {
@@ -23,33 +21,8 @@ const ResumeApi = [
     institute: "Mirpur Bangla High School",
     year: 2014,
     major: 'Science',
-    rate: "GPA 5.00/5.00",
     exam: "Secondary School Certificate (SSC)"
-  },
-  {
-    id: 4,
-    category: "experience",
-    year: "BSE In CSE (2004 - 2008) ",
-    title: "Diploma in Web Development",
-    desc: "Contrary to popular belief. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
-    rate: "4.70/5 ",
-  },
-  {
-    id: 5,
-    category: "experience",
-    year: "Job at Rainbow-Themes (2008 - 2016) ",
-    title: "The Personal Portfolio Mystery",
-    desc: "Generate Lorem Ipsum which looks. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
-    rate: "4.95/5 ",
-  },
-  {
-    id: 6,
-    category: "experience",
-    year: "Works at Plugin Development (2016 - 2020) ",
-    title: "Diploma in Computer Science",
-    desc: "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
-    rate: "5.00/5 ",
-  },
+  }
 ]
 
 export default ResumeApi
